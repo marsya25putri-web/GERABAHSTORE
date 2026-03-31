4d38a32614e2b1bd32a18abadd76e482ec0ed84a
marsya25putri-web <marsya25putri@gmail.com>
2026-03-31 09:39:47 +0700

    Fix: pindahkan login.css ke folder styles dan perbaiki import

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import "../dist/css/login.css";
+import "../styles/login.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
